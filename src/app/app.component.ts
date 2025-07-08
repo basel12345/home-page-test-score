777b445e69008772c3f163ebd7d2bc9605511f4c
basel12345 <baslelsawy@gmail.com>
2025-07-09 02:53:26 +0300

    add scroll to wit navigate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { SidebarService } from './services/sidebar.service';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,5 +17,14 @@ import { SidebarService } from './services/sidebar.service';
 export class AppComponent {
   title = 'Home-Test-Score-Page';
 
-  constructor(public sideBarService: SidebarService) { }
+  constructor(
+    public sideBarService: SidebarService,
+    private router: Router
+  ) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,7 +9,14 @@ import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      ...[
+        withInMemoryScrolling({
+          scrollPositionRestoration: 'enabled',
+        })
+      ]
+    ),
     provideClientHydration(),
     provideAnimationsAsync(),
     providePrimeNG({
