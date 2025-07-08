a35939e5dc4bb6332092540e60e2ee48e15f4be9
basel12345 <baslelsawy@gmail.com>
2025-07-08 12:14:42 +0300

    add cahnges

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { SidebarService } from './../../services/sidebar.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
 templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -14,6 +15,15 @@ export class NavbarComponent {
 
   toogleSideBar() {
     this.sidebarService.openSideBar = !this.sidebarService.openSideBar;
+  }
+
+  scrollTo(sectionId: string) {
+    const el = document.getElementById(sectionId);
+    if (el) el.scrollIntoView({ behavior:"smooth" });
+  }
+
+  login() {
+    window.open('https://testandscoreapi.github.io/TASUI/')
   }
 
 }
